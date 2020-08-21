ae6abab7871f81646c658ca5592b94d56d603c78
haiweilian <haiweilian@foxmail.com>
2020-08-21 21:44:52 +0800

    react-native root file App.tsx

--- React/react-native-study-demo/App.tsx
+++ React/react-native-study-demo/App.tsx
@@ -1,47 +1,20 @@
 import React from 'react';
-import {Alert} from 'react-native';
-import WebView from 'react-native-webview';
+import {Text, StyleSheet, View} from 'react-native';
 
-// 自己选个还有空场的电影页面
-const uri = 'https://m.mtime.cn/#!/onlineticket/592463544/';
-
-const INJECT_JS = (window, document) => {
-  // let submitBtn;
-  // function waitForBtnRender() {
-  //   submitBtn = document.getElementById('submitBtn');
-  //   if (!submitBtn) {
-  //     // 提交按钮渲染了吗？没有的话我过两秒再来问
-  //     setTimeout(waitForBtnRender, 2000);
-  //   } else {
-  //     submitBtn.onclick = () => {
-  //       const seats = [];
-  //       document.querySelectorAll('.seat_selected').forEach((node) => {
-  //         seats.push(node.getAttribute('name'));
-  //       });
-  //       window.ReactNativeWebView.postMessage(seats.join(', '));
-  //     };
-  //   }
-  // }
-  // waitForBtnRender();
-
-  // 里面有 jquery 直接使用事件委托绑定事件了
-  $(document).on('click', '#submitBtn', function () {
-    const seats = [];
-    $('.seat_selected').each(function () {
-      seats.push($(this).attr('name'));
-    });
-    window.ReactNativeWebView.postMessage(seats.join(', '));
-  });
-};
-
-export default function App() {
+function App() {
   return (
-    <WebView
-      source={{uri}}
-      injectedJavaScript={`(${INJECT_JS.toString()})(window, document)`}
-      onMessage={(e) => {
-        Alert.alert('您选中的座位是：' + e.nativeEvent.data);
-      }}
-    />
+    <View style={styles.container}>
+      <Text>把 src/routers 下的示例复制到 App.tsx 文件里查看效果</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
