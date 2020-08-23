8c2fce501e0f95c7ba028021c141dff8db1559a2
haiweilian <haiweilian@foxmail.com>
2020-08-23 11:06:15 +0800

    flutter 界面搭建

---
+++ Dart/flutter_study_demo/lib/pages/ChannelScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChannelScreen extends StatelessWidget {
+  const ChannelScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('频道'),
+      ),
+    );
+  }
+}
---
+++ Dart/flutter_study_demo/lib/pages/UserScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserScreen extends StatelessWidget {
+  const UserScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('我的'),
+      ),
+    );
+  }
+}
