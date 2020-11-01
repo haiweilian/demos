9a113723c55b32812d4c5a38e5817f41317907d3
haiweilian <haiweilian@foxmail.com>
2020-11-01 17:35:11 +0800

    Chrome 插件(扩展)开发

---
+++ PlugIn/chrome-plugin-demo/README.md
@@ -0,0 +1,3 @@
+# chorme 插件开发
+
+Chrome 插件(扩展)开发全攻略 <https://github.com/sxei/chrome-plugin-demo>
---
+++ PlugIn/chrome-plugin-demo/com.baidu.www/content-script.js
@@ -0,0 +1,23 @@
+﻿(function () {
+  console.log("这是 chrome-plugin-demo 的 content-script！");
+})();
+
+/**
+ * 注入脚本
+ * @param {String} jsPath 路径
+ */
+function injectScript(jsPath) {
+  var temp = document.createElement("script");
+  temp.setAttribute("type", "text/javascript");
+  temp.src = chrome.extension.getURL(jsPath);
+  document.head.appendChild(temp);
+}
+
+/**
+ * 开始注入
+ */
+// === 包含 jquery 的网站不需要
+// injectScript("utils/jquery.min.js");
+
+// === 交互功能入口
+injectScript("com.baidu.www/inject-script.js");
