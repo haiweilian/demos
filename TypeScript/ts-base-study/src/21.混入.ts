07a887c7409c3b38d00931401c8622d355a12363
haiweilian <haiweilian@foxmail.com>
2021-02-05 21:40:01 +0800

    ts 基础学习

--- TypeScript/ts-base-study/src/21.混入.ts
+++ TypeScript/ts-base-study/src/21.混入.ts
@@ -1,1 +1,47 @@
-// ====== 函数式编程的世界，不推荐混入
+// ====== 每个类都只定义了一个特定的行为或功能，创建一个新类，同时具有这两种功能
+// Disposable Mixin
+class Disposable {
+  isDisposed: boolean;
+  dispose() {
+    this.isDisposed = true;
+  }
+}
+
+// Activatable Mixin
+class Activatable {
+  isActive: boolean;
+  activate() {
+    this.isActive = true;
+  }
+  deactivate() {
+    this.isActive = false;
+  }
+}
+
+class SmartObject {
+  constructor() {
+    setInterval(() => console.log(this.isActive + " : " + this.isDisposed), 500);
+  }
+
+  interact() {
+    this.activate();
+  }
+}
+
+interface SmartObject extends Disposable, Activatable {}
+applyMixins(SmartObject, [Disposable, Activatable]);
+
+let smartObj = new SmartObject();
+setTimeout(() => smartObj.interact(), 1000);
+
+////////////////////////////////////////
+// In your runtime library somewhere
+////////////////////////////////////////
+
+function applyMixins(derivedCtor: any, baseCtors: any[]) {
+  baseCtors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name));
+    });
+  });
+}
