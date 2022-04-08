9fd4ecabd3f1100bb9e61fcf325f4f4edbcff042
haiweilian <haiweilian@foxmail.com>
2022-04-08 09:00:29 +0800

    svg && pwa 基础

---
+++ Graph/svg-base/README.md
@@ -0,0 +1,3 @@
+<https://juejin.cn/post/6844903589807128590>
+
+<https://developer.mozilla.org/zh-CN/docs/Web/SVG>
---
+++ JavaScript/pwa-base/src/sw.js
@@ -0,0 +1,4 @@
+// 这里处理缓存策略：一般用 workbox 处理
+self.addEventListener('fetch', function(event) {
+  console.log(event)
+});
