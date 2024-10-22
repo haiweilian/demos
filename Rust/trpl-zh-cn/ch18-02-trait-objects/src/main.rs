0c59074e46165f8876c39ee66ec9fb68030b9dfa
haiweilian <haiweilian@foxmail.com>
2024-10-22 16:55:08 +0800

    trpl-zh-cn

---
+++ Rust/trpl-zh-cn/ch18-02-trait-objects/src/main.rs
@@ -0,0 +1,63 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Button, {},{},{}", self.width, self.height, self.label)
+    }
+}
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!(
+            "SelectBox, {},{},{:#?}",
+            self.width, self.height, self.options
+        )
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+            // Box::new(String::from("Hi"))
+        ],
+    };
+    screen.run();
+}
