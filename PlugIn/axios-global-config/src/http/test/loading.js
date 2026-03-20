10eef0fdfd3a6caa870bca3315ae322d232ba3b4
haiweilian <haiweilian@foxmail.com>
2026-03-20 13:27:59 +0800

    rename

--- PlugIn/axios-global-config/src/http/test/loading.js
+++
@@ -1,8 +0,0 @@
-import http from '../http'
-
-/**
- * restful 测试
- */
-export function getUser (params, config = {}) {
-  return http.get('/users/haiweilian', params, 1, config)
-}
--- PlugIn/taggd-leader-line-marker/README.md
+++
@@ -1,5 +0,0 @@
-# taggd-leader-line-marker
-
-taggd、leader-line 创建标记和指引连接线
-
-![preview](./src/assets/preview.png)
